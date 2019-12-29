abd202c1e55107ea2daa09844dbfa170a0add15b
tiago emerick <tiagoeborsanyi@gmail.com>
2019-12-29 17:37:34 -0300

    add simple components users and places

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
-function App() {
-  return <h1>Let's start!</h1>;
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+
+const  App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Users} />
+        <Route path="/places/new" component={NewPlace} />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
