26fcd6bd41b3658c1f807a4384baacc44b4f1344
tiago emerick <tiagoeborsanyi@gmail.com>
2019-12-29 20:42:23 -0300

    create card user components

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-    return <h2>Users works2!</h2>;
+    const USERS = [
+        {
+            id: 'u1', 
+            name: 'pamela', 
+            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6KEzKL7hovYwKuB1RRKMk1Nm86NFH3XtFOstdqxvsy4nKhZfsrQ&s', 
+            places: 3
+        }
+    ];
+
+    return <UsersList items={USERS} />;
 };
 
 export default Users;
