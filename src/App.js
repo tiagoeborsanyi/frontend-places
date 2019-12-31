2dcc223a1be688af84fc550d88f4dc5f43bd290e
tiago emerick <tiagoeborsanyi@gmail.com>
2019-12-31 12:27:41 -0300

    add login form component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import Users from './user/pages/Users';
+import Auth from './user/pages/Auth';
 import NewPlace from './places/pages/NewPlace';
 import UpdatePlace from './places/pages/UpdatePlace';
 import UserPlaces from './places/pages/UserPlaces';
@@ -17,6 +18,7 @@ const  App = () => {
           <Route path="/:userId/places" component={UserPlaces} />
           <Route path="/places/new" exact component={NewPlace} />
           <Route path="/places/:placeId" component={UpdatePlace} />
+          <Route path="/auth" component={Auth} />
           <Redirect to="/" />
         </Switch>
       </main>
