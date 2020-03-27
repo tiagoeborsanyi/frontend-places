2ac19741d8868ff7c93dac408f28cfba81a095b3
Tiago Emerick <tiagoeborsanyi@gmail.com>
2020-03-27 12:25:23 -0300

    add components spinner and change component auth

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -3,6 +3,9 @@ import React, { useState, useContext } from 'react';
 import Card from '../../shared/components/UIElements/Card';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+
 import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
 import { AuthContext } from '../../shared/context/auth-context';
@@ -11,6 +14,8 @@ import './Auth.css';
 const Auth = () => {
     const auth = useContext(AuthContext);
     const [isLoginMode, setIsLoginMode] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
 
     const [formState, inputHandler, setFormData] = useForm({
         email: {
@@ -23,10 +28,36 @@ const Auth = () => {
         }
     }, false);
 
-    const authSubmitHandler = event => {
+    const authSubmitHandler = async event => {
         event.preventDefault();
-        console.log(formState.inputs);
-        auth.login();
+        
+        if (isLoginMode) {
+
+        } else {
+            try {
+                setIsLoading(true);
+                const response = await fetch('http://localhost:5000/api/users/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: formState.inputs.name.value,
+                    email: formState.inputs.email.value,
+                    password: formState.inputs.password.value
+                    })
+                });
+
+                const responseData = await response.json();
+                console.log(responseData);
+                setIsLoading(false);
+                auth.login();
+            } catch (error) {
+                console.log(error);
+                setError(error.message || 'Something went wrong.');
+            }
+        }
+        setIsLoading(false);
     }
 
     const switchModeHandler = () => {
@@ -49,6 +80,7 @@ const Auth = () => {
 
     return (
         <Card className="authentication">
+            {isLoading && <LoadingSpinner asOverlay />}
             <h2>Login Required</h2>
             <hr />
             <form onSubmit={authSubmitHandler}>
