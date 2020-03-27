6cbd6cf2db813e9699a9aaf2c8ac6cda559edf82
Tiago Emerick <tiagoeborsanyi@gmail.com>
2020-03-27 16:04:34 -0300

    load places by user id

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,40 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import PlaceList from '../components/PlaceList';
-
-const DUMMY_PLACES = [
-    {
-        id: 'p1', 
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world!',
-        imageUrl: 'https://www.parques-e-ingressos.com.br/sites/default/files/imagecache/512-384/theme-park/empire-state-building-tickets/empire_state_building_tickets.jpg',
-        address: '20 w 34th St, New York, NY 10001',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9856644
-        },
-        creator: 'u1'
-    },
-    {
-        id: 'p2', 
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world!',
-        imageUrl: 'https://www.parques-e-ingressos.com.br/sites/default/files/imagecache/512-384/theme-park/empire-state-building-tickets/empire_state_building_tickets.jpg',
-        address: '20 w 34th St, New York, NY 10001',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9856644
-        },
-        creator: 'u2'
-    }
-]
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
 const UserPlaces = () => {
-    const userId = useParams().userId;
-    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+    const [loadedPlaces, setLoadedPlaces] = useState();
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
-    return <PlaceList items={loadedPlaces} />
+    const userId = useParams().userId;
+
+    useEffect(() => {
+        const fetchPlaces = async () => {
+            const responseData = await sendRequest(`http://localhost:5000/api/places/user/${userId}`);
+            setLoadedPlaces(responseData.places);
+        };
+        fetchPlaces();
+    }, [sendRequest]);
+
+    return (
+        <React.Fragment>
+            <ErrorModal error={error} onClear={clearError} />
+            {isLoading && (
+                <div className="center">
+                    <LoadingSpinner />
+                </div>
+            )}
+            {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+        </React.Fragment>
+    )
 }
 
 export default UserPlaces;
